d9952de4af15737b03c197e20d7e2857249d9b58
juliosam <ja.samaniegom@gmail.com>
2022-05-09 15:03:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+      <Home/>
+        <h1>App</h1>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
